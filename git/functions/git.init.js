5591acf195d5f0da5f58fcf976f929db184c8ea7
Greg Fedirko <gfedirko@zagmail.gonzaga.edu>
2014-11-09 17:33:42 -0800

    git init functioning

--- git/functions/git.init.js
+++ git/functions/git.init.js
@@ -1,6 +1,14 @@
 // Create a .git directory inside the root directory
+var git = require('nodegit');
+var path = require('path');
+var fs = require('fs');
 
-module.exports = function(rootFilePath) {
-	console.log('git init!');
-
+module.exports = function(rootFilePath, callback) {
+	var resolvedPath = path.resolve(rootFilePath, '.git')
+	git.Repo.init(resolvedPath, false, function(initializeGitError, repo){
+		if (initializeGitError) {
+			throw initializeGitError;
+		}
+		console.log('Initialized empty git repository in ' + resolvedPath);
+	});
 }
