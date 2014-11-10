2ada205bf15e470ce83d0cf9ecdb6909bfe9c023
Greg Fedirko <gfedirko@zagmail.gonzaga.edu>
2014-11-09 17:17:03 -0800

    created a test file

---
+++ test.js
@@ -0,0 +1,9 @@
+var gitFunctions = require('./git/git.js');
+
+
+// Before testing, create a 'test' directory one level higher than 
+// this folder
+
+var rootFilePath = '../test';
+
+gitFunctions.init(rootFilePath);
