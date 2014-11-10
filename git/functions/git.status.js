5a10883bfa5300e18fb2cb4957726df0292cbb52
Greg Fedirko <gfedirko@zagmail.gonzaga.edu>
2014-11-09 19:50:11 -0800

    git.status working

--- git/functions/git.status.js
+++ git/functions/git.status.js
@@ -1,22 +1,16 @@
-var git = require('gift');
+var gift = require('gift');
 var path = require('path');
 var fs = require('fs');
 
 module.exports = function(rootFilePath) {
 
-	git.Repo.open(path.resolve(rootFilePath, '.git'), function(openRepoError, repo) {
-		if(openRepoError) {
-			throw openRepoError;
-		}
+	var repo = gift(rootFilePath);
 
-		repo.openIndex(function(openIndexError, index) {
-			if (openIndexError) {
-				throw openIndexError;
-			}
-
-			console.log(index.entries());
-
-		});
+	repo.status(function(err, status) {
+	 	if (err) {console.log(err);};
+	 	console.log(status);
+	 	return status;
 	});
+
 
 };
